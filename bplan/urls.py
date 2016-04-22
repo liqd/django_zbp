b2cecc97b94d2fce3a264d3ac4156690570372d5
Magdalena Noffke <magdalenanoffke@magmac.speedport_w_724v_typ_a_05011603_00_009>
2016-04-22 13:18:44 +0200

    add urls

--- bplan/urls.py
+++ bplan/urls.py
@@ -8,11 +8,13 @@ from .api import BPlanMultipolygonViewSet
 from .api import BPlanDataViewSet
 from .views import BezirkDetailView
 
+
 router = routers.DefaultRouter()
 router.register(r'bezirke', BezirkViewSet, base_name='bezirk')
 router.register(r'ortsteile', OrtsteilViewSet, base_name='ortsteil')
 router.register(r'bplaene_point', BPlanPointViewSet, base_name='bplan')
-router.register(r'bplaene_multipolygon', BPlanMultipolygonViewSet, base_name='bplan_multipoligon')
+router.register(r'bplaene_multipolygon',
+                BPlanMultipolygonViewSet, base_name='bplan_multipoligon')
 router.register(r'bplaene_data', BPlanDataViewSet, base_name='bplan_data')
 
 
@@ -21,5 +23,9 @@ urlpatterns = [
     url(r'^berlin/$',
         TemplateView.as_view(template_name="bplan/berlin.html"), name='berlin'),
     url(r'^berlin/(?P<slug>[a-z\-]+)/',
-        BezirkDetailView.as_view(), name='bezirk')
+        BezirkDetailView.as_view(), name='bezirk'),
+    # user login
+    url(r'^accounts/login/', 'bplan.views.login_user'),
+    url(r'^logout/', 'bplan.views.logout_user'),
+    url(r'^downloads/', 'bplan.views.downloads')
 ]
