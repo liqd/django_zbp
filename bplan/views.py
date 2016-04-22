1ddea84fe94f80ddfcb003e25ddcbd2087fd9089
Magdalena Noffke <magdalenanoffke@magmac.speedport_w_724v_typ_a_05011603_00_009>
2016-04-22 13:48:15 +0200

    add @xframe_options_exempt

--- bplan/views.py
+++ bplan/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Bezirk
+from django.views.generic import TemplateView
+from django.views.decorators.clickjacking import xframe_options_exempt
+
 
 class BezirkDetailView(DetailView):
 
-	model = Bezirk
+    model = Bezirk
+
+    @xframe_options_exempt
+    def dispatch(self, *args, **kwargs):
+        return super(BezirkDetailView, self).dispatch(*args, **kwargs)
 
 
+class StadtView(TemplateView):
+
+    @xframe_options_exempt
+    def dispatch(self, *args, **kwargs):
+        return super(StadtView, self).dispatch(*args, **kwargs)
