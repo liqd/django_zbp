64a78b2cd09768435d3bb9da4f571793b7dae079
Yves Müller <yves.mueller@liqd.de>
2016-04-22 17:12:37 +0200

    Enable django chaching middelware
     - use dummy cache (doesn't cache) for dev
     - use in memory cache for production

--- django_zbp/settings/dev.py
+++ django_zbp/settings/dev.py
@@ -9,6 +9,12 @@ SECRET_KEY = '*rg(3(8c!3zi$wu0b_q=al@dn#$sc44c^6rb9uy8$jc65abpms'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
 try:
     from .local import *
 except ImportError:
