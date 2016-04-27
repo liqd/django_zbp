c265c0fb15eab0c7d2c568a3ed2382c0fbde42ad
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-04-27 13:31:45 +0200

    update settings in order to show right dates

--- django_zbp/settings/base.py
+++ django_zbp/settings/base.py
@@ -103,15 +103,17 @@ COMPRESS_PRECOMPILERS = (
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de-de'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
+
+USE_L10N = True
 
 LOGIN_URL = '/login/'
 
