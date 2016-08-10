4901c3c61f528c9181f51bc34b52627f2f9be7d0
Magdalena Noffke <magdalena.noffke@liqd.de>
2016-08-10 14:26:23 +0200

    add bplan counts to bezirk api

--- bplan/serializers.py
+++ bplan/serializers.py
@@ -4,6 +4,8 @@ from .models import Bezirk
 from .models import Ortsteil
 from .models import BPlan
 from datetime import datetime
+from django.utils import timezone
+from django.contrib.gis.geos import Point
 
 
 class OrtsteilSerializer(GeoFeatureModelSerializer, serializers.HyperlinkedModelSerializer):
@@ -15,11 +17,53 @@ class OrtsteilSerializer(GeoFeatureModelSerializer, serializers.HyperlinkedModel
 
 
 class BezirkSerializer(GeoFeatureModelSerializer, serializers.HyperlinkedModelSerializer):
+    bplan_count = serializers.SerializerMethodField()
+    bplan_festgesetzt_count = serializers.SerializerMethodField()
+    bplan_imVerfahren_count = serializers.SerializerMethodField()
+    bplan_aul_count = serializers.SerializerMethodField()
+    bplan_bbg_count = serializers.SerializerMethodField()
+    point = serializers.SerializerMethodField()
 
     class Meta:
         model = Bezirk
-        fields = ('name', 'slug', 'polygon', 'ortsteile', 'pk')
+        fields = ('name', 'slug', 'polygon', 'ortsteile', 'pk',
+                  'bplan_count', 'bplan_festgesetzt_count',
+                  'bplan_imVerfahren_count', 'bplan_aul_count',
+                  'bplan_bbg_count', 'point')
         geo_field = 'polygon'
+
+    def get_bplan_count(self, object):
+        count = BPlan.objects.filter(bezirk=object).count()
+        return count
+
+    def get_bplan_festgesetzt_count(self, object):
+        return BPlan.objects.filter(bezirk=object).filter(festg=True).count()
+
+    def get_bplan_imVerfahren_count(self, object):
+        bplan_count = self.get_bplan_count(object)
+        bplan_festgesetzt_count = self.get_bplan_festgesetzt_count(object)
+        bplan_aul_count = self.get_bplan_aul_count(object)
+        bplan_bbg_count = self.get_bplan_bbg_count(object)
+        return bplan_count - bplan_festgesetzt_count - bplan_aul_count - bplan_bbg_count
+
+    def get_bplan_aul_count(self, object):
+        date = timezone.now().date()
+        qs = BPlan.objects.filter(bezirk=object).filter(festg=False)
+        qs = qs.exclude(aul_anfang__isnull=True, aul_ende__isnull=True)
+        count = qs.filter(aul_anfang__lte=date, aul_ende__gte=date).count()
+        return count
+
+    def get_bplan_bbg_count(self, object):
+        date = timezone.now().date()
+        qs = BPlan.objects.filter(bezirk=object).filter(festg=False)
+        qs = qs.exclude(bbg_anfang__isnull=True, bbg_ende__isnull=True)
+        count = qs.filter(bbg_anfang__lte=date, bbg_ende__gte=date).count()
+        return count
+
+    def get_point(self, object):
+        point = Point(
+            object.polygon.centroid.x, object.polygon.centroid.y, srid=4326)
+        return point.coords
 
 
 class SimpleBezirkSerializer(serializers.ModelSerializer):
@@ -31,7 +75,6 @@ class SimpleBezirkSerializer(serializers.ModelSerializer):
 
 class SimpleBPlanSerializer(serializers.ModelSerializer):
     status = serializers.SerializerMethodField()
-
 
     class Meta:
         model = BPlan
