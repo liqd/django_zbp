e1b478a673b88ad46de507f5f7a25201bb5bee4f
Robert Mader <r.mader@liqd.net>
2019-05-22 16:35:48 +0200

    webpack: Update config to use 'Terser'

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,18 @@
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const common = require('./webpack.config.js')
+const merge = require('webpack-merge')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
-  devtool: 'source-map',
-  plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ]
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true,
+        parallel: true,
+        terserOptions: {
+          ecma: 5,
+          arrows: false
+        }
+      })
+    ]
+  }
 })
