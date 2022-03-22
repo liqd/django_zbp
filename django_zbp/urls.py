8372e22888118641197d18effb0e9ae7aa74306b
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-03-22 13:38:50 +0100

    django update to 3.2: fix deprecation of django.conf.urls

--- django_zbp/urls.py
+++ django_zbp/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'', include('bplan.urls')),
-    url(r'^admin/', admin.site.urls)
+    path('', include('bplan.urls')),
+    path('admin/', admin.site.urls)
 ]
