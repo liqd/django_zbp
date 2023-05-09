974c0cf913f051f684822afaf442882ab055b8c5
Julian Dehm <j.dehm@liqd.net>
2023-05-09 10:44:40 +0200

    refactor broken python code in load_bplan
    * fix exception handling
    * remove invalid python code
    * add debug logging

--- bplan/management/commands/load_bplan.py
+++ bplan/management/commands/load_bplan.py
@@ -1,11 +1,7 @@
 import copy
 import os
-import sys
-import json
+from operator import itemgetter
 
-from tqdm import tqdm
-
-from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.contrib.gis.gdal import DataSource
 from django.contrib.gis.gdal.error import GDALException
@@ -13,20 +9,26 @@ from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.geos import MultiPolygon
 from django.contrib.gis.geos import Point
 from django.contrib.gis.geos import Polygon
-from django.contrib.gis.db import models
+from django.core.management.base import BaseCommand
+from tqdm import tqdm
 
-from bplan.models import Bezirk
 from bplan.models import BPlan
-from bplan.models import Ortsteil
+from bplan.models import Bezirk
 from bplan.models import Download
-
-from django.conf import settings
+from bplan.models import Ortsteil
 
 
 class Command(BaseCommand):
+    def __init__(self, stdout=None, stderr=None, no_color=False, force_color=False):
+        super().__init__(stdout, stderr, no_color, force_color)
+        self.planname = ""
+        self.debug = False
+
     def _check_freedom(self, point, points):
-        for other_point in points:
-            if point.distance(other_point) < 0.0001:
+        for k in range(len(points)):
+            if point.distance(points[k][1]) < 0.0001:
+                if self.debug:
+                    print("Centers of " + self.planname + " and " + points[k][0] + " are too close")
                 return False
         return True
 
@@ -41,11 +43,15 @@ class Command(BaseCommand):
 
     def _get_bounds(self, multipolygon):
         # we currently assume all multipolygons to actually be plain polygons
-        min_x = sort(multipolygon[0][0], lambda a, b: a[0] < b[0])[0]
-        max_x = sort(multipolygon[0][0], lambda a, b: a[0] > b[0])[0]
-        min_y = sort(multipolygon[0][0], lambda a, b: a[1] < b[1])[0]
-        max_y = sort(multipolygon[0][0], lambda a, b: a[1] > b[1])[0]
-        return [min_x, max_x, min_y, max_y]
+        min_x = sorted(multipolygon[0][0], key=itemgetter(0))[0][0]
+        max_x = sorted(multipolygon[0][0], key=itemgetter(0), reverse=True)[0][0]
+        min_y = sorted(multipolygon[0][0], key=itemgetter(1))[0][1]
+        max_y = sorted(multipolygon[0][0], key=itemgetter(1), reverse=True)[0][1]
+        bounds = [min_x, max_x, min_y, max_y]
+        if self.debug:
+            print("Bounds of " + self.planname + ":")
+            print(bounds)
+        return bounds
 
     def _get_next_pseudocentroid(self, k, half, multipolygon):
         a = multipolygon[0][0][k]
@@ -65,55 +71,46 @@ class Command(BaseCommand):
                     bounds[0] + (2 * i + 1) * d_x / (2 * k),
                     bounds[2] + (2 * i + 1) * d_y / (2 * k),
                     srid=4326))
-        return quadrants
+        return centers
 
     def _calculate_point(self, multipolygon, points):
         # Try three different methods for getting a unique, central point within the bplan polygon:
         # 1. the centroid
-        try:
-            point = Point(
-                multipolygon[0].centroid.x,
-                multipolygon[0].centroid.y,
-                srid=4326)
-            point_is_happy = self._check_inclusion_and_freedom(
-                multipolygon, point, points)
-            if point_is_happy:
-                return point
-            else:
-                k = 0
-                half = int(len(multipolygon[0][0]) / 2)
-                while k + 1 < half:
-                    # 2. a "pseudocentroid", i.e. the centroid of a simplified polygon
-                    try:
-                        new_point = self._get_next_pseudocentroid(
-                            k, half, multipolygon)
-                        new_point_is_happy = self._check_inclusion_and_freedom(
-                            multipolygon, new_point, points)
-                        if new_point_is_happy:
-                            return new_point
-                        else:
-                            k += 1
-                    except:
-                        k += 1
-
-                k = 2
-                bounds = self._get_bounds(multipolygon)
-                while k < 10:
-                    # 3. a "tile center", i.e. the center of a diagonal tile in k^2 square tiles
-                    try:
-                        new_points = self._get_next_centers(k, bounds)
-                        for new_point in new_points:
-                            new_point_is_happy = self._check_inclusion_and_freedom(
-                                multipolygon, new_point, points)
-                            if new_point_is_happy:
-                                return new_point
-                        k += 1
-                    except:
-                        k += 1
-
-                return Point(multipolygon[0][0][0], srid=4326)
-        except:
-            return Point(multipolygon[0][0][0], srid=4326)
+        point = Point(
+            multipolygon[0].centroid.x,
+            multipolygon[0].centroid.y,
+            srid=4326)
+        if self._check_inclusion_and_freedom(multipolygon, point, points):
+            if self.debug:
+                print("Found point for bplan: " + self.planname + " using method: centroid")
+            return point
+        else:
+            k = 0
+            half = int(len(multipolygon[0][0]) / 2)
+            while k + 1 < half:
+                # 2. a "pseudo-centroid", i.e. the centroid of a simplified polygon
+                new_point = self._get_next_pseudocentroid(
+                    k, half, multipolygon)
+                if self._check_inclusion_and_freedom(multipolygon, new_point, points):
+                    if self.debug:
+                        print("Found point for bplan: " + self.planname + " using method: pseudo-centroid")
+                    return new_point
+                else:
+                    k += 1
+            k = 2
+            bounds = self._get_bounds(multipolygon)
+            while k < 10:
+                # 3. a "tile center", i.e. the center of a diagonal tile in k^2 square tiles
+                new_points = self._get_next_centers(k, bounds)
+                for new_point in new_points:
+                    if self._check_inclusion_and_freedom(multipolygon, new_point, points):
+                        if self.debug:
+                            print("Found point for bplan: " + self.planname + " using method: tile center")
+                        return new_point
+                k += 1
+        if self.debug:
+            print("Failed to find a suitable point for bplan: " + self.planname + " , using fallback")
+        return Point(multipolygon[0][0][0], srid=4326)
 
     def _download_geodata(self, filename, url, layer):
         call = 'ogr2ogr -s_srs EPSG:25833' \
@@ -125,7 +122,7 @@ class Command(BaseCommand):
 
         try:
             os.remove(filename)
-        except:
+        except FileNotFoundError:
             pass
 
         result = os.system(call)
@@ -170,7 +167,7 @@ class Command(BaseCommand):
         b = feature.get("BEZIRK")
         bezirk = Bezirk.objects.get(name=b)
         bezirk_name = bezirk.name
-        return (bezirk, bezirk_name)
+        return bezirk, bezirk_name
 
     def _get_ortsteile(self, geometry):
         ortsteile = Ortsteil.objects.filter(polygon__intersects=geometry)
@@ -185,13 +182,13 @@ class Command(BaseCommand):
             afs_l_aend = feature.get("AFS_L_AEND").date
         except AttributeError:
             afs_l_aend = None
-        return (afs_beschl, afs_l_aend)
+        return afs_beschl, afs_l_aend
 
     def _get_festg_data(self, feature):
         try:
             f = feature.get("FESTSG")
             festg = True if f == 'ja' else False
-        except:
+        except AttributeError:
             festg = None
         try:
             festsg_von = feature.get("FESTSG_VON")
@@ -201,7 +198,7 @@ class Command(BaseCommand):
             festsg_am = feature.get("FESTSG_AM").date
         except AttributeError:
             festsg_am = None
-        return (festg, festsg_von, festsg_am)
+        return festg, festsg_von, festsg_am
 
     def _get_participation_data(self, feature):
         try:
@@ -220,7 +217,7 @@ class Command(BaseCommand):
             aul_ende = feature.get("AUL_ENDE").date
         except AttributeError:
             aul_ende = None
-        return (bbg_anfang, bbg_ende, aul_anfang, aul_ende)
+        return bbg_anfang, bbg_ende, aul_anfang, aul_ende
 
     def _get_www_data(self, feature):
 
@@ -232,47 +229,35 @@ class Command(BaseCommand):
             www = feature.get("AUSLEG_WWW")
             if www:
                 ausleg_www = www
-        except:
+        except AttributeError:
             pass
 
         try:
             scan = feature.get("SCAN_WWW")
             if scan:
                 scan_www = scan
-        except:
+        except AttributeError:
             pass
 
         try:
             grund = feature.get("GRUND_WWW")
             if grund:
                 grund_www = grund
-        except:
+        except AttributeError:
             pass
 
-        return (ausleg_www, scan_www, grund_www)
+        return ausleg_www, scan_www, grund_www
 
     def _get_other_data(self, feature):
-        try:
-            gml_id = feature.get("gml_id")
-        except:
-            gml_id = None
-        try:
-            fsg_gvbl_n = feature.get("FSG_GVBL_N")
-        except:
-            fsg_gvbl_n = None
-        try:
-            fsg_gvbl_s = feature.get("FSG_GVBL_S")
-        except:
-            fsg_gvbl_s = None
+        gml_id = feature.get("gml_id")
+        fsg_gvbl_n = feature.get("FSG_GVBL_N")
+        fsg_gvbl_s = feature.get("FSG_GVBL_S")
         try:
             fsg_gvbl_d = feature.get("FSG_GVBL_D").date
         except AttributeError:
             fsg_gvbl_d = None
-        try:
-            normkontr = feature.get("NORMKONTR")
-        except:
-            normkontr = None
-        return (gml_id, fsg_gvbl_n, fsg_gvbl_s, fsg_gvbl_d, normkontr)
+        normkontr = feature.get("NORMKONTR")
+        return gml_id, fsg_gvbl_n, fsg_gvbl_s, fsg_gvbl_d, normkontr
 
     def add_arguments(self, parser):
 
@@ -283,11 +268,20 @@ class Command(BaseCommand):
             default=False,
             help='Load data from fixtures')
 
+        parser.add_argument(
+            '--debug',
+            action='store_true',
+            dest='debug',
+            default=False,
+            help='Print detailed info about plans')
+
     def handle(self, *args, **options):
 
         url = 'http://fbinter.stadt-berlin.de/fb/' \
               'wfs/data/senstadt/sach_bplan'
 
+        if options['debug']:
+            self.debug = True
         if options['fromFixtures']:
             fixtures_dir = os.path.join(settings.BASE_DIR, 'bplan', 'fixtures')
             fixture_file = os.path.join(fixtures_dir, 'bplan.geojson')
@@ -304,6 +298,7 @@ class Command(BaseCommand):
 
         errors = []
         points = []
+        plannames = []
 
         for feature in tqdm(
                 data_source[0], disable=(int(options['verbosity']) < 1)):
@@ -315,13 +310,15 @@ class Command(BaseCommand):
                 pass
 
             if geometry:
-
-                planname = self._get_identifier(feature)
-                bplanID = planname.replace(" ", "").lower()
+                self.planname = self._get_identifier(feature)
+                if self.debug and self.planname in plannames:
+                    print("warning: duplicate bplan: " + self.planname)
+                plannames.append(self.planname)
+                bplan_id = self.planname.replace(" ", "").lower()
                 multipolygon, multipolygon_25833, geometry, bereich = self._get_spatial_data(
                     feature)
                 point = self._calculate_point(multipolygon, points)
-                points.append(point)
+                points.append((self.planname, point))
                 ortsteile = self._get_ortsteile(geometry)
                 afs_behoer = feature.get("AFS_BEHOER")
                 afs_beschl, afs_l_aend = self._get_imVerfahren_data(feature)
@@ -335,9 +332,9 @@ class Command(BaseCommand):
                 try:
                     bezirk, bezirk_name = self._get_district(feature)
                     bplan, created = BPlan.objects.update_or_create(
-                        bplanID=bplanID,
+                        bplanID=bplan_id,
                         defaults={
-                            'planname': planname,
+                            'planname': self.planname,
                             'multipolygon': multipolygon,
                             'multipolygon_25833': multipolygon_25833,
                             'point': point,
@@ -370,8 +367,8 @@ class Command(BaseCommand):
                         bplan.download = download
                         bplan.save()
                 except Exception as e:
-                    newError = "Bplan " + planname + ": " + str(e)
-                    errors.append(newError)
+                    new_error = "Bplan " + self.planname + ": " + str(e)
+                    errors.append(new_error)
                     pass
 
         download.errors = errors
